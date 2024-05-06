41607383622ac1f71c24b7a8b5c2d6ee6729cdfa
ddingdong <ddingdong@ddingdongs-MacBook-Pro.local>
2024-05-06 22:39:28 +0900

    기본 구조 세팅

---
+++ app/server.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse, JSONResponse, HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List, Union
+from langserve.pydantic_v1 import BaseModel, Field
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langserve import add_routes
+#from app.response_chain import response_chain
+from app.consult_chain import consult_chain, retriever
+#from app.chat import chain as chat_chain
+
+from starlette.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+#from consult_chain import consult_chain, retriever 
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+
+#app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
+
+#get 메서드로 "/" 경로에 접속하면, 아래의 함수를 실행하라.
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/mystery")
+
+@app.get("/mystery", response_class=HTMLResponse)
+async def chat_page(request: Request, query: str = None):  # 쿼리 파라미터를 받는 부분 추가
+    # 쿼리 파라미터가 있는 경우, 해당 쿼리로 처리하여 답변 반환
+    if query:
+        try:
+            docs = retriever.invoke(query)
+            answer = consult_chain.invoke({"question": query, "personality": docs})
+            return JSONResponse(content={"answer": answer})
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+    # 쿼리 파라미터가 없는 경우, 일반 chat 페이지 반환
+    else:
+        return templates.TemplateResponse("chat.html", {"request": request})
+    
+@app.post("/mystery")
+async def get_answer(user_input):
+    try:
+        docs = retriever.invoke(user_input)
+        answer = consult_chain.invoke({"question": user_input, "personality" : docs})
+
+        return JSONResponse(content={"answer": answer})
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
