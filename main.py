ab213fd9bd8539b0e5bf8867512921b82a835ef8
ddingdong <ddingdong@ddingdongs-MacBook-Pro.local>
2024-05-07 13:03:23 +0900

    requirements.txt 추가, main.py 생년월일 input 추가

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from loguru import logger
 from langchain.chains import ConversationalRetrievalChain
 from langchain_openai import ChatOpenAI
 
-from langchain.document_loaders import PyPDFLoader
-from langchain.document_loaders import Docx2txtLoader
-from langchain.document_loaders import UnstructuredPowerPointLoader
+#from langchain.document_loaders import PyPDFLoader
+#from langchain.document_loaders import Docx2txtLoader
+#from langchain.document_loaders import UnstructuredPowerPointLoader
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
@@ -15,12 +15,14 @@ from langchain.memory import ConversationBufferMemory
 #from langchain.vectorstores import FAISS
 
 # from streamlit_chat import message
-from langchain.callbacks import get_openai_callback
+#from langchain.callbacks import get_openai_callback
 from langchain.memory import StreamlitChatMessageHistory
 
 #new added
 from dotenv import load_dotenv
 import os
+from datetime import date
+
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
@@ -46,10 +48,10 @@ openai = ChatOpenAI(model_name="gpt-3.5-turbo-0125",
 
 def main():
     st.set_page_config(
-    page_title="DirChat",
-    page_icon=":books:")
+    page_title="사주풀이",
+    page_icon=":four_leaf_clover:")
 
-    st.title("_Private Data :red[QA Chat]_ :books:")
+    st.title(":four_leaf_clover:_여러분의 타고난 성격이 궁금하신가요?_ :four_leaf_clover:")
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
@@ -59,6 +61,9 @@ def main():
 
     if "processComplete" not in st.session_state:
         st.session_state.processComplete = None
+
+    dob = st.sidebar.date_input("생년월일을 입력해주세요.", value=None, min_value=date(1900, 1, 1), max_value=date.today())
+
 
     # with st.sidebar:
     #     uploaded_files =  st.file_uploader("Upload your file",type=['pdf','docx'],accept_multiple_files=True)
@@ -105,9 +110,25 @@ def main():
 
     st.session_state.processComplete = True
 
-    if 'messages' not in st.session_state:
-        st.session_state['messages'] = [{"role": "assistant", 
-                                        "content": "안녕하세요! 주어진 문서에 대해 궁금하신 것이 있으면 언제든 물어봐주세요!"}]
+    #생일 입력을 위함.
+    # 세션 상태에 'dob_entered' 키가 없으면 초기화
+    if 'dob_entered' not in st.session_state:
+        st.session_state['dob_entered'] = False
+
+    # 사용자가 생년월일을 입력했는지 확인
+    if dob is not None and not st.session_state['dob_entered']:
+        # 생년월일 입력 처리
+        print("\n\ndebug")
+        st.session_state['dob_entered'] = True
+        st.session_state['dob'] = dob
+
+    # session_state를 사용하여 메세지 상태 관리
+    if 'messages' not in st.session_state or st.session_state['dob_entered']:
+        # 생년월일이 입력되지 않았다면, 입력 요청 메세지 추가
+        if not st.session_state['dob_entered']:  
+            st.session_state['messages'] = [{"role": "user", "content": "사이드바를 열고, 생년월일을 먼저 입력해주세요!"}]
+        else:
+            st.session_state['messages'] = [{"role": "assistant", "content": f"안녕하세요! 궁금하신 것이 있으면 언제든 물어봐주세요! 당신의 생년월일은 {dob}입니다."}]
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
@@ -141,7 +162,7 @@ def main():
                 #     st.markdown(source_documents[0].metadata['source'], help = source_documents[0].page_content)
                 #     st.markdown(source_documents[1].metadata['source'], help = source_documents[1].page_content)
                 #     st.markdown(source_documents[2].metadata['source'], help = source_documents[2].page_content)
-                    
+
 
 
 # Add assistant message to chat history
@@ -152,27 +173,27 @@ def tiktoken_len(text):
     tokens = tokenizer.encode(text)
     return len(tokens)
 
-def get_text(docs):
+# def get_text(docs):
 
-    doc_list = []
+#     doc_list = []
     
-    for doc in docs:
-        file_name = doc.name  # doc 객체의 이름을 파일 이름으로 사용
-        with open(file_name, "wb") as file:  # 파일을 doc.name으로 저장
-            file.write(doc.getvalue())
-            logger.info(f"Uploaded {file_name}")
-        if '.pdf' in doc.name:
-            loader = PyPDFLoader(file_name)
-            documents = loader.load_and_split()
-        elif '.docx' in doc.name:
-            loader = Docx2txtLoader(file_name)
-            documents = loader.load_and_split()
-        elif '.pptx' in doc.name:
-            loader = UnstructuredPowerPointLoader(file_name)
-            documents = loader.load_and_split()
+#     for doc in docs:
+#         file_name = doc.name  # doc 객체의 이름을 파일 이름으로 사용
+#         with open(file_name, "wb") as file:  # 파일을 doc.name으로 저장
+#             file.write(doc.getvalue())
+#             logger.info(f"Uploaded {file_name}")
+#         if '.pdf' in doc.name:
+#             loader = PyPDFLoader(file_name)
+#             documents = loader.load_and_split()
+#         elif '.docx' in doc.name:
+#             loader = Docx2txtLoader(file_name)
+#             documents = loader.load_and_split()
+#         elif '.pptx' in doc.name:
+#             loader = UnstructuredPowerPointLoader(file_name)
+#             documents = loader.load_and_split()
 
-        doc_list.extend(documents)
-    return doc_list
+#         doc_list.extend(documents)
+#     return doc_list
 
 
 def get_text_chunks(text):
@@ -185,14 +206,14 @@ def get_text_chunks(text):
     return chunks
 
 
-def get_vectorstore(text_chunks):
+#def get_vectorstore(text_chunks):
     # embeddings = HuggingFaceEmbeddings(
     #                                     model_name="jhgan/ko-sroberta-multitask",
     #                                     model_kwargs={'device': 'cpu'},
     #                                     encode_kwargs={'normalize_embeddings': True}
     #                                     )  
     #vectordb = FAISS.from_documents(text_chunks, embeddings)
-    return vectordb
+    #return vectordb
 
 def get_conversation_chain(vetorestore,openai_api_key):
     llm = ChatOpenAI(openai_api_key=openai_api_key, model_name = 'gpt-3.5-turbo',temperature=0)
