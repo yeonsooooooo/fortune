4d36f70e76eefb5e68dc4d940a7131f0325e38f7
ddingdong <ddingdong@ddingdongs-MacBook-Pro.local>
2024-05-22 02:07:15 +0900

    temp

--- main.py
+++ main.py
@@ -317,28 +317,6 @@ def tiktoken_len(text):
     tokens = tokenizer.encode(text)
     return len(tokens)
 
-# def get_text(docs):
-
-#     doc_list = []
-    
-#     for doc in docs:
-#         file_name = doc.name  # doc 객체의 이름을 파일 이름으로 사용
-#         with open(file_name, "wb") as file:  # 파일을 doc.name으로 저장
-#             file.write(doc.getvalue())
-#             logger.info(f"Uploaded {file_name}")
-#         if '.pdf' in doc.name:
-#             loader = PyPDFLoader(file_name)
-#             documents = loader.load_and_split()
-#         elif '.docx' in doc.name:
-#             loader = Docx2txtLoader(file_name)
-#             documents = loader.load_and_split()
-#         elif '.pptx' in doc.name:
-#             loader = UnstructuredPowerPointLoader(file_name)
-#             documents = loader.load_and_split()
-
-#         doc_list.extend(documents)
-#     return doc_list
-#dsfdfsfd
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
